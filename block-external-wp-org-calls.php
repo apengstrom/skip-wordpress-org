8ce7d3be81e14896fd1f38da9b6cddb466b802df
apengstrom <andrew@curiousm.com>
2024-09-26 22:40:12 -0400

    Make some logic adjustments to save plugins.php face

--- block-external-wp-org-calls.php
+++ block-external-wp-org-calls.php
@@ -1,13 +1,18 @@
 <?php
 /**
  * Plugin Name: Skip WP ORG API Calls
+ * Plugin URI: https://github.com/apengstrom/skip-wordpress-org
  * Description: Block Matt, Eloquently
- * Author:      Andrew Engstrom
- * License:     GNU General Public License v3 or later
- * License URI: http://www.gnu.org/licenses/gpl-3.0.html
+ * Version: 1.0.0
+ * Author: Andrew Engstrom
+ * Author URI: https://www.curiousm.com/
+ * License: GPLv3
+ * License URI: https://www.gnu.org/licenses/gpl-3.0.html
  */
 
-// Basic security, prevents file from being loaded directly.
+/*
+ * Basic security, prevents file from being loaded directly.
+ */
 defined( 'ABSPATH' ) or die( 'Not today!' );
 
 /*
@@ -79,10 +84,22 @@ add_filter('pre_http_request', 'disable_wp_org_endpoints', 10, 3);
  */
 function filter_http_request_args($args, $url) {
 
-    // James bond this request
+    // Filter the URL
     if (strpos($url, 'wordpress.org') != false || strpos($url, 'woocommerce.com') != false) {
-        add_action('admin_notices','add_admin_warning');
-        return false;
+
+        $current_screen = '';
+        if ( function_exists( 'get_current_screen' ) ) {
+            $current_screen = get_current_screen();
+        }
+
+        // James bond this request if it's ajax
+        if ((defined('DOING_AJAX') && DOING_AJAX) || $current_screen->id != 'plugins') {
+            die("Another day!");
+        } else {
+            // We are likely hitting this on something like plugins.php, so let's show a friendly message.
+            add_action('admin_notices','add_admin_warning');
+            return false;
+        }
     }
 
     return $args;
